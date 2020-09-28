fd37676ef951de17aca74397f1648fae612b3b02
Luis <62965678+Tiquillo@users.noreply.github.com>
2020-09-27 20:54:10 -0600

    más comentarios de documentación

--- src/EnviaMensaje.java
+++ src/EnviaMensaje.java
@@ -3,11 +3,26 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
+/** Envía el mensaje mediante sockets
+ * @author Luis Delgado
+ * @version 1.2
+ * @since 0.4
+ */
+
 public class EnviaMensaje {
 
     DataInputStream in;
     DataOutputStream out;
     Socket socket;
+
+    /** Envía el mensaje a la dirección de loopback
+     * 
+     * @param host de destino
+     * @param puerto de destino
+     * @param mensaje
+     * @param puerto de origen
+     * @return valor buleano
+     */
 
     public boolean Enviar(String hostDestino, int puertoDestino, String mensaje, int puertoOrigen){
 
